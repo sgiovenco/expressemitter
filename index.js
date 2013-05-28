cc368b3bb4d74e7b4484ad986292a1a551ac4786
sgiovenco <sgiovenco@gmail.com>
2013-05-28 02:28:35 -0400

    Initial Commit.  Alpha version.

---
+++ index.js
@@ -0,0 +1,59 @@
+(function expressemitter_index_js() {
+
+  var isInteger = RegExp.prototype.test.bind(/^\d+$/);
+
+  Array.clone = function clone(arrayLikeObj) {
+    var a = [];
+    Object.keys(arrayLikeObj).filter(isInteger).forEach(function(i) {
+      a[i] = arrayLikeObj[i];
+    });
+    return a;
+  };
+
+  Function.curry = function curry(/*arg0, arg1, ..., argN-1*/) {
+    var fn = this, curriedArgs = Array.clone(arguments);
+    return function curriedFn(/*argN, argN+1, ...*/) {
+      return fn.apply(this, curriedArgs.concat(Array.clone(arguments)));
+    };
+  };
+
+  this.exports = function SSEmitter() {
+    return function ssemitter(request, response, nextHandler) {
+      var nextEventId = 0
+        , statusCode = 200
+        , headers = {
+          "Cache-Control": "no-cache",
+          "Connection": "keep-alive",
+          "Content-Type": "text/event-stream",
+          "Trailer": "Content-Type"
+        };
+      (request.connection || request.socket).setTimeout(Infinity);
+      response.writeHead(statusCode, headers);
+
+      response.sendEvent = function sendEvent(ev, arg) {
+        var sseMessage = ["event: "+ev, "id: "+nextEventId++]
+          , args = Array.clone(arguments).slice(1);
+        if (args.length) {
+          sseMessage.push.apply(sseMessage, args.map(function(line) {
+            return "data: "+(typeof line==="object"?JSON.stringify(line):line);
+          }));
+        }
+        else {
+          sseMessage.push("data");
+        }
+        sseMessage = sseMessage.concat(["", ""]).join("\n");
+        console.log("Sending message: "+JSON.stringify(sseMessage));
+        this.write(sseMessage);
+      };
+
+      var ogEnd = response.end;
+      response.end = function end() {
+        this.statusCode = statusCode = 404;
+        this.addTrailers(headers = {"Content-Type": "application/x-empty"});
+        ogEnd.apply(this, arguments);
+      };
+
+      nextHandler();
+    };
+  };
+}).apply(module, [].map(require));
